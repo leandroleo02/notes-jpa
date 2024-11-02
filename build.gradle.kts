2fbfe9d20c6de844ba9a000ef7064ae6cb763357
Leandro Rodrigues <leandroleo02@gmail.com>
2024-11-02 14:51:54 -0300

    Update libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     idea
-	java
-	id("org.springframework.boot") version "3.3.5"
-	id("io.spring.dependency-management") version "1.1.3"
-	id("org.graalvm.buildtools.native") version "0.9.20"
+    java
+    id("org.springframework.boot") version "3.3.5"
+    id("io.spring.dependency-management") version "1.1.6"
+    id("org.graalvm.buildtools.native") version "0.10.3"
 }
 
 group = "br.com.notacaoozona"
@@ -18,23 +18,23 @@ idea {
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-hateoas")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.flywaydb:flyway-core")
-	implementation("org.flywaydb:flyway-database-postgresql")
-	compileOnly("org.projectlombok:lombok:1.18.34")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-hateoas")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql")
+    compileOnly("org.projectlombok:lombok:1.18.34")
     annotationProcessor("org.projectlombok:lombok:1.18.34")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.postgresql:postgresql")
@@ -42,16 +42,21 @@ dependencies {
     annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	implementation("io.hypersistence:hypersistence-utils-hibernate-63:3.8.2")
-    implementation("javax.transaction:javax.transaction-api:1.3")
-    implementation("org.hibernate:hibernate-jcache:6.5.1.Final")
-    implementation("org.ehcache:ehcache:3.10.8")
-    implementation("org.glassfish.jaxb:jaxb-runtime:2.3.1")
+    implementation("io.hypersistence:hypersistence-utils-hibernate-63:3.8.2")
+    implementation("jakarta.transaction:jakarta.transaction-api:2.0.1")
+    implementation("org.hibernate.orm:hibernate-jcache:6.6.1.Final")
+    implementation("org.ehcache:ehcache:3.10.8") {
+        capabilities {
+            requireCapability("org.ehcache:ehcache-jakarta")
+        }
+    }
+    implementation("org.glassfish.jaxb:jaxb-runtime:4.0.5")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks.named<Jar>("jar") {
